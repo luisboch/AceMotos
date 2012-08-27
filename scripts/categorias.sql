33de45bc040c7b5a072c032ed078f494bf065149
luis <luis@mint-notebook>
2012-08-27 18:54:41 -0300

    formatação

--- scripts/categorias.sql
+++ scripts/categorias.sql
@@ -1,1 +1,6 @@
-create table categorias(id integer auto_increment primary key, descricao varchar(255), categoria_id integer, constraint fk_cat_parent foreign key(categoria_id) references categorias(id));
+create table categorias(
+    id integer auto_increment primary key,
+    descricao varchar(255), 
+    categoria_id integer, 
+    constraint fk_cat_parent foreign key(categoria_id) references categorias(id)
+);
--- scripts/produtos_fotos.sql
+++ scripts/produtos_fotos.sql
@@ -1,1 +1,8 @@
- create table produtos_fotos (id integer primary key auto_increment, produto_id integer, ordem integer, tamanho integer, caminho varchar(255), legenda varchar(255));
+create table produtos_fotos (
+    id integer primary key auto_increment, 
+    produto_id integer, 
+    ordem integer, 
+    tamanho integer, 
+    caminho varchar(255), 
+    legenda varchar(255)
+);
